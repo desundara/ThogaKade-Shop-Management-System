bfc53206855115b072ea1ec6406893f3fb381c80
Gayani Samaraweera <156066338+desundara@users.noreply.github.com>
2025-10-02 20:40:42 +0530

    add Customer class & DBConnection class

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,23 @@
+package model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Customer {
+
+    private String id;
+    private String title;
+    private String name;
+    private LocalDate dob;
+    private Double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
+}
